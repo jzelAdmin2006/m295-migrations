a823c7567f85690b514b65603f0a17dc21dfde58
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-05-23 13:45:14 +0200

    add partial task 1

---
+++ database/migrations/2023_05_23_112726_create_reservations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('note')->nullable();
+            $table->date('date');
+            $table->integer('nights');
+            $table->decimal('room_temperature', 3, 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
